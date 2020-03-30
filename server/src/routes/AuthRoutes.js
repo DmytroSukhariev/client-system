743ec3eed2bfd40be7152dc456d68c58c7f08286
Artem Osuskii <rexshoot692@gmail.com>
2020-03-30 21:37:55 +0300

    email check init

--- server/src/routes/AuthRoutes.js
+++ server/src/routes/AuthRoutes.js
@@ -2,30 +2,15 @@ const {Router} = require('express')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const config = require('config')
-const {check, validationResult} = require('express-validator')
 const User = require('../models/User')
+const emailPattern = /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
 
 const router = Router()
 
 
-    router.post(
-    '/register', 
-    [
-        check('email', 'Неверный формат').isEmail(),
-        check('password', 'Минимальная длина пароля - 8 символов').isLength({min: 8})
-            
-    ],
-    async (req, res) =>{
+    router.post('/register', async (req, res) => {
+    
     try{
-        const errors = validationResult(req)
-
-        if (!errors.isEmpty){
-            return res.status(400).json({
-                errors: errors.array(),
-                message: 'Некорректные данные при регистрации'
-            })
-        }
-
         const {firstName, secondName, email, password} = req.body
         
         const candidate = await User.findOne({ email })
@@ -35,25 +20,32 @@ const router = Router()
         }
 
         const hashPass = await bcrypt.hash(password, 12)
-        const user = new User({firstName, secondName, email, password: hashPass})
+        const user = new User({
+            firstName,
+            secondName, 
+            email, 
+            password: hashPass
+        })
+        const validEmail = emailPattern.test(email)
+    
+
+        if(!validEmail){
+            return res.status(400).json({message: "Неверный формат"})
+        }
 
         await user.save()
 
         res.status(201).json({message: "Пользователь создан"})
 
     } catch (e){
-        res.status(500).json( {message: "Что-то пошло не так"} )
-        throw new Error(e)
+         res.status(500).json({message: "Что-то пошло не так"})
+         throw new Error(e)
     }
-    })
+})
 
 
     router.post(
     '/login', 
-    [
-        check('email', 'Некорректный формат').normalizeEmail().isEmail(),
-        check('password', 'Введите пароль').exists()
-    ],
     async (req, res) => {
     try{
         // const errors = validationResult(req)
